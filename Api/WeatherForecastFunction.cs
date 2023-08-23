42745f626972af978dafb8561d929e222eb1b643
mkarmark <mikarmar@microsoft.com>
2023-08-22 22:00:39 -0700

    Update WeatherForecastFunction.cs

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,76 +1,29 @@
 using System;
 using System.IO;
-using System.Linq;
-using System.Net;
-using BlazorApp.Shared;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace ApiIsolated
 {
-    public class HttpTrigger
+    public static class HttpTrigger
     {
-        private readonly ILogger _logger;
-
-        public HttpTrigger(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<HttpTrigger>();
-        }
-
-        [Function("WeatherForecast")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
-        {
-            var randomNumber = new Random();
-            var temp = 0;
-
-            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
-                Summary = GetSummary(temp)
-            }).ToArray();
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.WriteAsJsonAsync(result);
-
-            return response;
-        }
-
-        [Function("GetCsv")]
-        public IActionResult RunGetCsvAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestData req)
+        [FunctionName("GetCsv")]
+        public static async Task<IActionResult> RunGetCsvAsync(
+                    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
         {
             var stream = new MemoryStream();
             var writer = new StreamWriter(stream);
             writer.Write("hello world!");
             writer.Flush();
             stream.Position = 0;
-            
+        
             return new FileStreamResult(stream, "text/csv")
             {
                 FileDownloadName = $"Sample-{DateTime.Today.ToString("yyyy-MM-dd")}.csv",
             };
-        }
-
-        private string GetSummary(int temp)
-        {
-            var summary = "Mild";
-
-            if (temp >= 32)
-            {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing";
-            }
-
-            return summary;
         }
     }
 }
